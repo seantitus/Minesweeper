0d1a7e019bc76dc545a8dfa0fa7fc85f8d85afe1
Sean Titus <stitus7@gatech.edu>
2016-09-08 14:53:35 -0400

    Added game over dialog

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -8,6 +8,7 @@ import javafx.event.EventHandler;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.Node;
+import javafx.scene.control.Dialog;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.Scene;
@@ -19,7 +20,7 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Minesweeper extends Application {
-    public void updateBoard(Board board, GridPane grid, Text flagText) {
+    private void updateBoard(GridPane grid, Board board, Text flagText) {
         for (Node node : grid.getChildren()) {
             Button btn = (Button) node;
             Square sq = board.getSquare(grid.getRowIndex(node), grid.getColumnIndex(node));
@@ -29,6 +30,19 @@ public class Minesweeper extends Application {
             }
         }
         flagText.setText("Flags: " + board.getFlags() + "/" + board.getMines());
+    }
+    private void endGame(boolean won, GridPane grid, Board board, Text flagText) {
+        ButtonType newGameButton = new ButtonType("New Game", ButtonBar.ButtonData.OK_DONE);
+        Dialog<ButtonType> over = new Dialog<>();
+        over.setContentText(won ? "You win!" : "You lose");
+        over.setTitle("Game Over");
+        over.getDialogPane().getButtonTypes().add(newGameButton);
+        over.showAndWait().ifPresent(response -> {
+            if (response == newGameButton) {
+                initialize(grid, board, flagText);
+            }
+        });
+
     }
     private void initialize(GridPane grid, Board board, Text flagText) {
         for (int i = 0; i < board.getHeight(); i++) {
@@ -45,19 +59,29 @@ public class Minesweeper extends Application {
                         if (event.getEventType().equals(MouseEvent.MOUSE_CLICKED)) {
                             if (event.isControlDown() || event.getButton() == MouseButton.SECONDARY) {
                                 board.flag(fi, fj);
+                                updateBoard(grid, board, flagText);
                             } else {
                                 if (!board.click(fi, fj)) {
-                                    System.out.println("u lose");
+                                    updateBoard(grid, board, flagText);
+                                    Board temp = new Board(board.getMines(), board.getWidth(),
+                                            board.getHeight());
+                                    endGame(false, grid, temp, flagText);
+                                    updateBoard(grid, temp, flagText);
                                 } else if (board.isWon()) {
-                                    System.out.println("u win yey");
+                                    updateBoard(grid, board, flagText);
+                                    Board temp = new Board(board.getMines(), board.getWidth(),
+                                            board.getHeight());
+                                    endGame(true, grid, temp, flagText);
+                                    updateBoard(grid, temp, flagText);
+                                } else {
+                                    updateBoard(grid, board, flagText);
                                 }
                             }
-                            updateBoard(board, grid, flagText);
                         }
                     }
                 });
                 grid.add(btn, j, i);
-                updateBoard(board, grid, flagText);
+                updateBoard(grid, board, flagText);
             }
         }
     }
